f76824dcdc916cc2eb5ac9e2011c54357c3708f6
SRHembree <75818806+SRHembree@users.noreply.github.com>
2021-07-02 16:59:56 -0400

    Finished page!!!!

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 function myFunction() {
-    var x = document.getElementById("hamburger")
+    var x = document.getElementById("navbar-collapse")
     if (x.style.display === "block") {
         x.style.display = "none"; 
     } else {
@@ -14,21 +14,3 @@ function newFunction() {
         x.style.display = "block"
     }
 }
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//     //Get the button:
-//   var tocsection = document.getElementById("tocsection");
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     tocsection.style.display = "block";
-//   } 
-//   else {
-//     tocsection.style.display = "none";
-//   }
-// }
-// // // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTo({ top: 0, behavior: 'smooth' }); // For Safari
-//     document.documentElement.scrollTo({ top: 0, behavior: 'smooth' }); // For Chrome, Firefox, IE and Opera
-// }
